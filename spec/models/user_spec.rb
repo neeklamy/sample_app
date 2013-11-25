96c948cd26997a25741db82541da7c0b14b091ab
Christopher Corbettis <neekalpha+gh@gmail.com>
2013-11-25 18:55:35 +0000

    Password and password_confirmation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,15 @@ require 'spec_helper'
 
 describe User do
 
-	before { @user = User.new(name: "Example User", email: "user@example.org") }
+	before { @user = User.new(name: "Example User", email: "user@example.org", password: "foobar", password_confirmation: "foobar") }
 
 	subject { @user }
 
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
+	it { should respond_to(:password_digest) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
 
 	it { should be_valid }
 
@@ -53,6 +56,20 @@ describe User do
 			user_with_same_email.save
 		end
 
+		it { should_not be_valid }
+	end
+
+	describe "when password is not present" do
+		before do
+			@user.password = " "
+			@user.password_confirmation = " "
+		end
+
+		it { should_not be_valid }
+	end
+
+	describe "when password doesn't match confirmation" do
+		before { @user.password_confirmation = "mismatch" }
 		it { should_not be_valid }
 	end
 end
