c2134e498ec67c0475da109768d65b45303b5391
Christopher Corbettis <neekalpha+gh@gmail.com>
2013-12-11 22:02:35 +0000

    Test user is signed in before updating

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -98,4 +98,24 @@ describe "User pages" do
       specify { expect(user.reload.email).to eq new_email }
     end
   end
+
+  describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { patch user_path(user) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+    end
+  end
 end
